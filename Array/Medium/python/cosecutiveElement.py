373c7d0c34e56623a86b8a8413f7a46c7638dd29
Prashant Dwivedi <prashant.dwivedi@routemobile.com>
2024-10-11 19:16:15 +0530

    consEle & matrix

--- Array/Medium/python/cosecutiveElement.py
+++ Array/Medium/python/cosecutiveElement.py
@@ -1,19 +1,76 @@
 """
-Longest Consecutive Sequence in an Array
-Problem Statement: You are given an array of ‘N’ integers. You need to find the length of the longest sequence which contains the consecutive elements.
+Longest Consecutive Sequence in an aay
+Problem Statement: You are given an aay of ‘N’ integers. You need to find the length of the longest sequence which contains the consecutive elements.
 """
 
-def findCons(arr):
-    left = right = 0
-    arr.sort()
-    maxi = 1
-    curr = 0
-    for i in range(len(arr)-1):
-        if(arr[i+1]-arr[i] == 1):
-            curr+=1
-            maxi = max(maxi,curr)
-        else:
-            curr = 0
-    print(maxi+1)
-arr =   [3, 8, 5, 7, 6]
-findCons(arr)
+a= [100,102,100,101,101,4,3,2,3,2,1,1,1,2]
+n=len(a)
+'''
+Brute Force
+TC: O(N^2)
+SC: O(N)
+'''
+import math
+def linearSearch(a, ele):
+    for i in range(len(a)):
+        if(a[i] == ele):
+            return 1
+    return 0
+
+def findConsecutiveElement(a):
+    maxi=-math.inf
+    for i in range(n):
+        ele=a[i]
+        count=0
+        while(linearSearch(a, ele)):
+            count+=1
+            ele+=1
+        maxi = max(maxi, count)
+    return maxi
+print('The longest consecutive using brute force approach: ', findConsecutiveElement(a))
+
+'''
+Better Approach: (Sort & search)
+TC: O(N * NlogN) 
+SC: O(N)
+'''
+def longestConsecutiveBetter(a):
+    import math
+    a.sort()
+    n=len(a)
+    lastSmaller = -math.inf
+    count=0
+    longest=-math.inf
+    for i in range(n):
+        if(a[i]-1 == lastSmaller):
+            count+=1
+            lastSmaller=a[i]
+        elif a[i] != lastSmaller:
+            count=1
+            lastSmaller = a[i]
+        longest = max(longest, count)
+    return longest
+
+print('The longest consecutive using better approach: ', longestConsecutiveBetter(a))
+
+'''
+Optimal Approach using set
+'''
+def findCons(a):
+    n = len(a)
+    if n == 0:
+        return 0
+    longest = 1
+    st = set()
+    for i in range(n):
+        st.add(a[i])
+    
+    for ele in st:
+        if ele-1 not in st:
+            count=1
+            while(ele+1 in st):
+                count+=1
+                ele+=1
+            longest= max(longest, count)
+    return longest
+print('The longest consecutive using better approach: ', findCons(a))
--- Array/Medium/python/matrixZero.py
+++ Array/Medium/python/matrixZero.py
@@ -2,42 +2,69 @@
 Set Matrix Zero
 Problem Statement: 
     Given a matrix if an element in the matrix is 0 then you will have to set its entire column and row to 0 and then return the matrix.
+TC: O(n*m) * (O(n) + O(m)) + O(n*m) 
+SC: O(n) + O(m)
 '''
 
-def matrix(arr,r,c):
-    temp = arr
-    for i in range( 0,r):
-        for j in range(0,c):
-            if arr[i][j] == 0:
-                # arr = makeRowZero(arr,i,j)
-                for row in range(0,r):
-                    temp[row][i] = -1
-                for col in range(0,c):
-                    temp[j][col] = -1
-    for i in range( 0,r):
-        for j in range(0,c):
-            if arr[i][j] == -1:
-                temp[i][j] = 0
-    return temp
+def matrix(a,row,col):
+    def markRow(i):
+        for j in range(col):
+            if(a[i][j] != 0):
+                a[i][j] = -1
 
-# def makeRowZero(arr,rows,columns):
-#     for column in range(0,columns):
-#         arr[rows][columns] = 0
-#         return arr
+    def markCol(j):
+        for i in range(row):
+            if(a[i][j] != 0):
+                a[i][j]=-1
 
-# def makeColumnZero(arr,rows,columns):
-#     for i in range(0,rows):
-#         arr[rows][columns] = 0
-#         return arr
+    for i in range(row):
+        for j in range(col):
+            if(a[i][j] == 0):
+                markRow(i)
+                markCol(j)
+
+    for i in range(row):
+        for j in range(col):
+            if(a[i][j] == -1):
+                a[i][j]=0
+
+    for i in range(row):
+        for j in range(col):
+            print(a[i][j], end= ' ')
+        print()
 
 arr = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+print("The Final matrix  using bruteForce is:")
 n = len(arr)
 m = len(arr[0])
 ans = matrix(arr, n, m)
 
-print("The Final matrix is:")
-for row in ans:
-    for ele in row:
-        print(ele, end=" ")
-    print(end="\n")
 
+
+'''
+Better
+TC
+SC
+'''
+
+def betterSol(arr, r, c):
+    rows = [0] * r
+    cols= [0] * c
+    for i in range(r):
+        for j in range(c):
+            if arr[i][j] == 0:
+                rows[i] = 1
+                cols[j] =1
+    
+    for i in range(r):
+        for j in range(c):
+            if(rows[i] or cols[j]):
+                arr[i][j] ==0
+    
+    for i in range(r):
+        for j in range(c):
+            print(arr[i][j], end= ' ')
+        print()
+
+print("The Final matrix  using better Solution is:")
+ans = betterSol(arr, n, m)
